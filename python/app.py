1e263303adeda28ad4ae47c527d5a4b8a4cccad9
Palhanoo <bpalhano00@gmail.com>
2022-02-01 11:41:35 -0300

    retornando apenas o necessario

--- python/app.py
+++ python/app.py
@@ -1,29 +1,31 @@
-import requests
+from array import array
+import json, requests
 from flask import Flask
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
 
-# def users():
-#     return {"users": [{"name": "Bruno Palhano", "location": "rua Pedro Juscelino de Aquino", "picture": "https://randomuser.me/api/portraits/men/1.jpg"},
-#         {"name": "Vitor", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/2.jpg"}, 
-#         {"name": "Vinicius", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/3.jpg"}, 
-#         {"name": "Thales", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/4.jpg"}, 
-#         {"name": "João", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/5.jpg"}, 
-#         {"name": "Yonara", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/women/2.jpg"}, 
-#         {"name": "Larissa", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/women/3.jpg"}, 
-#         {"name": "Elder", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/6.jpg"}, 
-#         {"name": "Marye", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/women/4.jpg"}, 
-#         {"name": "Ray", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/7.jpg"}, 
-#         {"name": "Felipe", "location": "Moovery Jp", "picture": "https://randomuser.me/api/portraits/men/8.jpg"}
-#         ]}
-
 @app.route("/")
 def buscar_dados():
-    request = requests.get("https://api.randomuser.me/?results=10")
-    dados = request.json()
-    return dados
+    response = requests.get("https://api.randomuser.me/?results=10")
+    dados = json.loads(response.content);
+    dados_tratados = tratar_dados(dados["results"]);
+    return json.dumps(dados_tratados)
+
+def tratar_dados(objeto):
+    new_dado = [];
+    for i in range(10):
+        del objeto[i]["cell"];
+        del objeto[i]["email"];
+        del objeto[i]["phone"];
+        del objeto[i]["nat"];
+        del objeto[i]["registered"];
+        del objeto[i]["login"]
+        del objeto[i]["id"]
+        del objeto[i]["gender"]
+        
+    return objeto
 
 if __name__ == "__main__":
-    app.run(port=3002)
+    app.run(port=3000)
--- python/main.py
+++ python/main.py
@@ -1,8 +1,22 @@
-import requests
+import requests, json
 
 def buscar_dados():
-    request = requests.get("https://api.randomuser.me/?results=10")
-    return(request.content)
+    response = requests.get("https://api.randomuser.me/?results=10")
+    dados = json.loads(response.content);
+    dados_tratados = tratar_dados(dados["results"]);
+    # print(dados["results"][0]["dob"]) 
+    print(dados_tratados);
 
+def tratar_dados(objeto):
+    new_dado = [];
+    for i in range(10):
+        # print(objeto[i]) 
+        del objeto[i]["dob"]
+        # new_dado["age"] = objeto[i]["dob"]["age"]
+        # new_dado["first_name"] = objeto[i]["name"]["first"]
+        # new_dado["last_name"] = objeto[i]["name"]["last"]
+        # new_dado["picture"] = objeto[i]["picutres"]
+        
+        return objeto
 if __name__ == '__main__':
     buscar_dados()
